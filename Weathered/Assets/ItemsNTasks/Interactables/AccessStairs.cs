babf6f9de74c1f7b4411dcf64e7641ee992bfd12
MikeNIke56 <111089840+MikeNIke56@users.noreply.github.com>
2023-10-26 03:18:59 -0400

    Stairs Access after Box Sort is Completed
    stairs will take player to an upstairs or downstairs position depending on where they are clicking from

---
+++ Weathered/Assets/ItemsNTasks/Interactables/AccessStairs.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AccessStairs : Interaction
+{
+    public bool isPassable = false;
+    bool isDown = true;
+    bool isUp = false;
+
+    public Transform upStairsSpawnPos;
+    public Transform downStairsSpawnPos;
+
+    PlayerController player;
+
+    private void Awake()
+    {
+        player = FindAnyObjectByType<PlayerController>();
+    }
+
+    public override void onClick()
+    {
+        if (isPassable == true)
+        {
+            if(isDown == true)
+            {
+                player.transform.position = upStairsSpawnPos.position;
+                isDown = false;
+                isUp = true;
+                Debug.Log("moved upstairs");
+            }
+            else
+            {
+                player.transform.position = downStairsSpawnPos.position;
+                isDown = true;
+                isUp = false;
+                Debug.Log("moved downstairs");
+            }
+        }
+    }
+}
