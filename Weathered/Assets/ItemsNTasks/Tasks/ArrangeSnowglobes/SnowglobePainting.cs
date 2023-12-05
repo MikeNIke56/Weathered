02dc41a712eb11ca36b6e4fd46ca60292c8596fb
MikeNIke56 <111089840+MikeNIke56@users.noreply.github.com>
2023-12-05 03:44:02 -0500

    Snowglobes Update

--- Weathered/Assets/ItemsNTasks/Tasks/ArrangeSnowglobes/SnowglobePainting.cs
+++ Weathered/Assets/ItemsNTasks/Tasks/ArrangeSnowglobes/SnowglobePainting.cs
@@ -2,17 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SnowglobePainting : Interaction
 {
     public GameObject investigateObjectPrefab;
     [SerializeField] string paintingName;
+    [SerializeField] Sprite inspectImg;
     public override void onClick()
     {
         ObservationMenu.observeMenu.ClearVisualRoot();
-        Instantiate(investigateObjectPrefab, ObservationMenu.observeMenu.visualRoot.transform);
+        var spawnObj = Instantiate(investigateObjectPrefab, ObservationMenu.observeMenu.visualRoot.transform);
         ObservationMenu.observeMenu.SetDescText("");
         ObservationMenu.observeMenu.nameText.text = paintingName;
+        spawnObj.GetComponent<Image>().sprite = inspectImg;
         UIController.UIControl.OpenInteractionMenu();   
     }
 }
