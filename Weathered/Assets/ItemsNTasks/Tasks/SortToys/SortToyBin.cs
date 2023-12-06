354c660354d41f3adecafeded75dde5384d430b1
HFedorsh <hfedorsh@gmu.edu>
2023-12-05 20:56:26 -0500

    SortToysAndScissors

---
+++ Weathered/Assets/ItemsNTasks/Tasks/SortToys/SortToyBin.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SortToyBin : Interaction
+{
+    SortToys sortTask;
+    [SerializeField] List<Item> validToys;
+
+    public override void onClick()
+    {
+        if (sortTask == null)
+        {
+            sortTask = FindFirstObjectByType<SortToys>();
+        }
+
+        sortTask.ClickBin(this);
+    }
+
+    public bool CheckToy(Item toyCheck)
+    {
+        if (validToys.Contains(toyCheck))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
