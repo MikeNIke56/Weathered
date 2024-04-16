382350380fe8897c47abca8cbe27ab74523bfb9a
ColeWiltzius <cwiltziu@gmu.edu>
2024-04-16 18:05:11 -0400

    Volume Sliders
    There are 4 sliders, but ambient sound slider is disabled because adjusting it started to play a sounds that should not be playing.

---
+++ Weathered/Assets/Scripts/General/Menus/AudioController.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioController : MonoBehaviour
+{
+    [SerializeField] private AudioMixer audioMixer;
+    [SerializeField] private string volumeGroup;
+
+    public void SetVolume(float sliderValue)
+    {
+        audioMixer.SetFloat(volumeGroup, Mathf.Log10(sliderValue) * 20);
+    }
+}
